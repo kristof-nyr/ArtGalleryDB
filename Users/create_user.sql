2163eee4eacad910be1d14630525e91219c2b562
kristof-nyr <kristof-nyr@DESKTOP-SFH5EMO>
2022-01-03 13:18:57 +0100

    Fixed a problem with user deletion

--- Users/create_user.sql
+++ Users/create_user.sql
@@ -1,36 +1,37 @@
-declare
-  cursor cur is
-    select 'alter system kill session ''' || sid || ',' || serial# || '''' as command
-      from v$session
-     where username = 'gallery_manager';
-begin
-  for c in cur loop
+DECLARE
+  CURSOR cur IS
+    SELECT 'alter system kill session ''' || sid || ',' || serial# || '''' AS command
+      FROM v$session
+     WHERE lower(username) = 'gallery_manager';
+BEGIN
+  FOR c IN cur
+  LOOP
     EXECUTE IMMEDIATE c.command;
-  end loop;
-end;
+  END LOOP;
+END;
 /
 
-DECLARE
-  v_count NUMBER;
+DECLARE v_count NUMBER;
 BEGIN
-  SELECT COUNT(*) INTO v_count FROM dba_users t WHERE t.username='gallery_manager';
-  IF v_count = 1 THEN 
+  SELECT COUNT(*)
+    INTO v_count
+    FROM dba_users t
+   WHERE lower(t.username) = 'gallery_manager';
+  IF v_count = 1
+  THEN
     EXECUTE IMMEDIATE 'DROP USER gallery_manager CASCADE';
   END IF;
 END;
-/
-CREATE USER Gallery_Manager 
-  IDENTIFIED BY "12345678" 
-  DEFAULT TABLESPACE users
-  QUOTA UNLIMITED ON users
-;
+/ 
 
-GRANT CREATE SESSION TO gallery_manager;
-GRANT CREATE TABLE TO gallery_manager;
-GRANT CREATE VIEW TO gallery_manager;
-GRANT CREATE SEQUENCE TO gallery_manager;
-GRANT CREATE PROCEDURE TO gallery_manager;
-GRANT CREATE TYPE TO gallery_manager;
-GRANT CREATE TRIGGER TO gallery_manager;
+CREATE USER gallery_manager identified BY "12345678" DEFAULT tablespace users quota unlimited ON users;
 
-ALTER SESSION SET CURRENT_SCHEMA=gallery_manager;
+grant CREATE session TO gallery_manager;
+grant CREATE TABLE TO gallery_manager;
+grant CREATE view TO gallery_manager;
+grant CREATE sequence TO gallery_manager;
+grant CREATE PROCEDURE TO gallery_manager;
+grant CREATE TYPE TO gallery_manager;
+grant CREATE TRIGGER TO gallery_manager;
+
+ALTER session SET current_schema = gallery_manager;
