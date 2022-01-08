86d0994e2b30d8613438fdc1fd25ba916b9f4d58
kristof-nyr <kristof-nyr@DESKTOP-SFH5EMO>
2022-01-08 15:36:19 +0100

    Expanded functions and types

---
+++ Functions/fn_galleries_by_city.sql
@@ -0,0 +1,29 @@
+﻿CREATE OR REPLACE FUNCTION fn_galleries_by_city(p_city VARCHAR2)
+  RETURN ty_gallery_list IS
+  v_galleries ty_gallery_list;
+BEGIN
+  SELECT ty_gallery(gallery_name => g.gallery_name,
+                    city         => a.city,
+                    street       => a.street,
+                    house_number => a.house_number)
+    BULK COLLECT
+    INTO v_galleries
+    FROM galleries g
+    JOIN address a
+      ON g.address_id = a.address_id
+   WHERE a.city LIKE '%' || p_city || '%';
+
+  --FOR i IN 1 .. v_galleries.count                                             _____________
+  --LOOP                                                                       |  TESTING    |
+   -- dbms_output.put_line(v_galleries(i)                                      |  PURPOSES   |
+   --                      .gallery_name || ' ' || v_galleries(i).city);        _____________
+  --END LOOP;
+
+  RETURN v_galleries;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    pkg_error_log.error_log(p_error_message => dbms_utility.format_error_backtrace,
+                            p_error_value   => 'p_city: ' || p_city,
+                            p_error_cause   => 'function: fn_galleries_by_city');
+END;
---
+++ Types/ty_gallery.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR REPLACE TYPE ty_gallery IS OBJECT(
+       gallery_name varchar2(50),
+       city varchar2(50),
+       street varchar2(50),
+       house_number number
+);
+/
+CREATE OR REPLACE TYPE ty_gallery_list IS TABLE OF ty_gallery;
